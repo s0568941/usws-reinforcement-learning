9cbe118260e699337131137807f6e168a50d460b
Nassim Uhrmann <nassim.uhrmann@sap.com>
2020-12-11 19:59:06 +0100

    Add constants file

---
+++ usws_jump_and_run_game/utils/constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Konstanten
+SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1380, 690
+JUMP_VELOCITY = 8
+STARTING_POSITION = 10
+PLAYER_STATIC_X = (SCREEN_WIDTH // 2)
